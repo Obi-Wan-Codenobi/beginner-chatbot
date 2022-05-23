758c6b21f91ebcd3da5271cd789386e1ea6c0b9a
Obi-Wan-Codenobi <Kade.singleton02@gmail.com>
2022-05-23 16:35:26 -0500

    curerntly an error with database. Saving and re-opening

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
 #IMPORTING and CREATING SQLite DATABASE
-
+#
 import sqlite3
 import json
 from datetime import datetime #this logs the time
+print ("***IMPORTING AND CREATING SQLITE DATABASE***")
 #
 #
 # Next, create code structure that executes one 
@@ -12,3 +13,21 @@ from datetime import datetime #this logs the time
 #
 timeframe = '2015-05'
 sqlite3.SQLITE_TRANSACTION = []
+print ("CREATING SINGLE SQL CODE STRUCTURE")
+#
+#
+# Connecting the data...
+# Download your dataset and get ready to connect it
+# connection = sqlite3.connect('PATH_NAME_OF_DATA/{}.db'.format(timeframe))
+#
+connection = sqlite3.connect('C:\\Users\Admin\Desktop\learning python AI\VSCODE\Beginner-chatbot\DATA\extracted data\RC_2015-05\{}.db'.format(timeframe))
+c = connection.cursor()
+#
+#
+# creating parent_id_, comment_id, body, name, and score into a table
+#
+#def create_table(): 
+ #   c.execute("CREATE TABLE IF IT DOES NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
+#def format_data(data):
+ #   data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
+ #   return data
